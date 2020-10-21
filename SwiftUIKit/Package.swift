99619e2bd28be7628f1db14325f5ce8efd697048
Relamquad <kkalivod@gmail.com>
2020-10-21 13:31:40 +0300

    update

--- SwiftUIKit/Package.swift
+++ SwiftUIKit/Package.swift
@@ -5,12 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUIKit",
-    platforms: [
-        .iOS(.v13),         //.v8 - .v13
-        .macOS(.v10_15),    //.v10_10 - .v10_15
-        .tvOS(.v13),        //.v9 - .v13
-        .watchOS(.v6),      //.v2 - .v6
-    ], products: [
+    products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIKit",
