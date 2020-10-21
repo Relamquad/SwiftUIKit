03145e882c4c7e6f8e9bb58eb42ca601a5aa9dd7
Relamquad <kkalivod@gmail.com>
2020-10-21 15:17:19 +0300

    minor

--- Sources/SwiftUIKit/ShareSheet.swift
+++ Sources/SwiftUIKit/ShareSheet.swift
@@ -15,10 +15,6 @@ public struct ShareSheet: UIViewControllerRepresentable {
     private var excludedActivityTypes: [UIActivity.ActivityType]?
     private var callback: Callback?
     
-    public init(activityItems: [Any]) {
-        self.activityItems = activityItems
-    }
-    
     public init(activityItems: [Any], applicationActivities: [UIActivity]? = nil, excludedActivityTypes: [UIActivity.ActivityType]? = nil, callback: Callback? = nil) {
         self.activityItems = activityItems
         self.applicationActivities = applicationActivities
