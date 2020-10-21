f42e0611696ac7b700c4c80783651c731be6712a
Relamquad <kkalivod@gmail.com>
2020-10-21 16:17:50 +0300

    update

--- Sources/SwiftUIKit/ShareSheet.swift
+++ Sources/SwiftUIKit/ShareSheet.swift
@@ -10,18 +10,18 @@ import SwiftUI
 public struct ShareSheet: UIViewControllerRepresentable {
     public typealias Callback = (_ activityType: UIActivity.ActivityType?, _ completed: Bool, _ returnedItems: [Any]?, _ error: Error?) -> Void
     
-    private var activityItems: [Any] = []
-    private let applicationActivities: [UIActivity]?
-    private let excludedActivityTypes: [UIActivity.ActivityType]?
-    private let callback: Callback?
+    var activityItems: [Any] = []
+    let applicationActivities: [UIActivity]?
+    let excludedActivityTypes: [UIActivity.ActivityType]?
+    let callback: Callback?
     
     public init(activityItems: [Any], applicationActivities: [UIActivity]? = nil, excludedActivityTypes: [UIActivity.ActivityType]? = nil, callback: Callback? = nil) {
-    
         self.activityItems = activityItems
         self.applicationActivities = applicationActivities
         self.excludedActivityTypes = excludedActivityTypes
         self.callback = callback
     }
+    
     
     public func makeUIViewController(context: Context) -> UIActivityViewController {
         let controller = UIActivityViewController(
